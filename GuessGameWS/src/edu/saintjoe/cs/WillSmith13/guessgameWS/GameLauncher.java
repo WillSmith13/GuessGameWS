c2142834f673e812420f6d949e87d4adc599f4b0
wills <wills@localhost>
2016-02-15 14:19:40 -0500

    GameLauncher class put in

---
+++ GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgameWS/GameLauncher.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.WillSmith13.guessgameWS;
+
+import edu.saintjoe.cs.WillSmith13.guessgame.GuessGame;
+
+public class GameLauncher {
+	public static void main (String[] arg) {
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+
+}
