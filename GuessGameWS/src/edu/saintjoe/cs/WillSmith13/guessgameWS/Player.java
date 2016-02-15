14718997768e391b85660094bbd3e117b4386257
wills <wills@localhost>
2016-02-15 14:13:17 -0500

    Almost there

---
+++ GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgameWS/Player.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.WillSmith13.guessgameWS;
+
+public class Player {
+	int number = 0; // where the guess goes
+	public void guess()
+	{
+		number = (int) (Math.random() + 10);
+		System.out.println("I'm guessing " + number);
+	}
+
+}
