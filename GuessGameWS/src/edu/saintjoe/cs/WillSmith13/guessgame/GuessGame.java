ecf34dbaceed6a1426d8143247ceb672c1e3f5ac
wills <wills@localhost>
2016-02-14 23:54:32 -0500

    Problems

--- GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgame/GuessGame.java
+++ GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgame/GuessGame.java
@@ -1,5 +1,43 @@
 package edu.saintjoe.cs.WillSmith13.guessgame;
 
 public class GuessGame {
-	// Here is where I will put my code later 
+	Player p1;
+	Player p2;
+	Player p3;
+	
+	public void startGame() { 
+	p1 = new Player ();
+	p2 = new Player ();
+	p3 = new Player ();
+	
+	int guessp1 = 0;
+	int guessp2 = 0;
+	int guessp3 = 0;
+	
+	boolean p1isRight = false;
+	boolean p2isRight = false;
+	boolean p3isRight = false;
+	
+	int targetNumber = (int) (Math.random() * 10);
+	System.out.println("I'm thinking of the a number between 0 and 9...");
+	
+	while(true) {
+		System.out.println("Number to guess is " + targetNumber);
+		
+		p1.guess();
+		p2.guess();
+		p3.guess();
+		
+		guessp1 = p1.number;
+		System.out.println("Player one guessed " + guessp1);
+		
+		guessp2 = p2.number;
+		System.out.println("Player two guessed" + guessp2);
+		
+		guessp3 = p3.number;
+		System.out.println("Player three guessed" + guessp3);
+		
+		
+	}
+}
 }
