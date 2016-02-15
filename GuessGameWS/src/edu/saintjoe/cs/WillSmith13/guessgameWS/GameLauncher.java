cd9f96172139b6ffead8bc337c63c5b36efbbeac
wills <wills@localhost>
2016-02-15 14:31:08 -0500

    All problems solved

--- GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgameWS/GameLauncher.java
+++ GuessGameWS/src/edu/saintjoe/cs/WillSmith13/guessgameWS/GameLauncher.java
@@ -1,7 +1,5 @@
 package edu.saintjoe.cs.WillSmith13.guessgameWS;
 
-import edu.saintjoe.cs.WillSmith13.guessgame.GuessGame;
-
 public class GameLauncher {
 	public static void main (String[] arg) {
 		GuessGame game = new GuessGame();
